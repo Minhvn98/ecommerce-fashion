0124ec67f16d8c8cc95d705e993467759b675942
Minhvn98 <minhvn62@wru.vn>
2021-07-11 22:43:00 +0700

    Connect api, complete detail page

---
+++ client/src/utils/slug.util.js
@@ -0,0 +1,37 @@
+const generateSlugs = (str, categoryId, productId) => {
+  // Chuyển hết sang chữ thường
+  str = str.toLowerCase();
+
+  // xóa dấu
+  str = str
+    .normalize("NFD") // chuyển chuỗi sang unicode tổ hợp
+    .replace(/[\u0300-\u036f]/g, ""); // xóa các ký tự dấu sau khi tách tổ hợp
+
+  // Thay ký tự đĐ
+  str = str.replace(/[đĐ]/g, "d");
+
+  // Xóa ký tự đặc biệt
+  str = str.replace(/([^0-9a-z-\s])/g, "");
+
+  // Xóa khoảng trắng thay bằng ký tự -
+  str = str.replace(/(\s+)/g, "-");
+
+  // Xóa ký tự - liên tiếp
+  str = str.replace(/-+/g, "-");
+
+  // xóa phần dư - ở đầu & cuối
+  str = str.replace(/^-+|-+$/g, "");
+
+  // return
+  return `${str}.${categoryId}.${productId}`;
+};
+
+const getCategoryIdAndProductId = slug => {
+  if (!slug) return;
+  const result = slug.split(".");
+  const len = result.length;
+
+  return [result[len - 2], result[len - 1]];
+};
+
+export { generateSlugs, getCategoryIdAndProductId };
