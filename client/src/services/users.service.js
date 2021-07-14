f2fe9c62ec99199d15d57c18581bc5181a2ea79f
khanhdev <levankhanh.dev@gmail.com>
2021-07-14 23:55:10 +0700

    fix conflict

--- client/src/services/users.service.js
+++ client/src/services/users.service.js
@@ -4,12 +4,16 @@ const BASE_URL = process.env.VUE_APP_BASE_URL;
 const loginHandler = (username, password) => {
   console.log(username, password);
   axios
-    .post(BASE_URL + "/login", {
-      username,
-      password
-    })
+    .post(
+      BASE_URL + "/login",
+      {
+        username,
+        password
+      },
+      { withCredentials: true }
+    )
     .then(function(response) {
-      console.log(response);
+      console.log(response["Set-Cookie"]);
     })
     .catch(function(error) {
       console.log(error);
